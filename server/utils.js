89bab2c699a82787feac6204449647ef62c7ac32
Matthew Go <matthewaarongo@gmail.com>
2017-08-16 19:58:29 -0400

    refactoring location apis

--- server/utils.js
+++ server/utils.js
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
+
 const axios = require('axios');
 const gmaps = require('./google-maps.js');
 const yelp = require('./yelp.js');
 
+
 const config = require('./config.js');
 
 const APIKEY = config.google.APIKEY;
 
-// Convert Latitude and Longitude from user's current location to address promise object
-=======
-const config = require('./config.js');
-const APIKEY = config.google.APIKEY;
-const axios = require('axios');
-
->>>>>>> rebase
 module.exports.LatLngToAddress = (coordinates) => {
   const lat = coordinates.coords.latitude;
   const long = coordinates.coords.longitude;
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${APIKEY}`;
   return axios.get(url);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> rebase
