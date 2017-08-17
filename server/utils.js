b855362f38069a495b385db26f80370a0954aabd
Matthew Go <matthewaarongo@gmail.com>
2017-08-16 20:06:05 -0400

    rebase

--- server/utils.js
+++ server/utils.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const axios = require('axios');
 const gmaps = require('./google-maps.js');
 const yelp = require('./yelp.js');
@@ -7,9 +8,19 @@ const config = require('./config.js');
 const APIKEY = config.google.APIKEY;
 
 // Convert Latitude and Longitude from user's current location to address promise object
+=======
+const config = require('./config.js');
+const APIKEY = config.google.APIKEY;
+const axios = require('axios');
+
+>>>>>>> rebase
 module.exports.LatLngToAddress = (coordinates) => {
   const lat = coordinates.coords.latitude;
   const long = coordinates.coords.longitude;
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${APIKEY}`;
   return axios.get(url);
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> rebase
