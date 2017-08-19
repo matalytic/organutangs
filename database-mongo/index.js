e54ea6889f484dcc2dfc94409136a7a933ca5d48
Wei Chen <tengwei1@gmail.com>
2017-08-19 12:50:23 -0400

    Revert "swapped to mlab, re-tries to connect to db every 5s when fails."
    This reverts commit ebf0353c5e3198eacc1ad898b1ab76283b3d9766.

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,28 +1,15 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://halfwaze:halfwaze@ds151153.mlab.com:51153/halfwaze', {useMongoClient: true});
+mongoose.connect('mongodb://localhost/test');
+
 var db = mongoose.connection;
 
-var connectIntervalId;
-
-db.on('error', () => {
-  console.log('mongoose connection error, retrying in 5 seconds');
-
-  connectIntervalId = setTimeout(() => {
-    connect();
-  }, 5000);
-
-  db.close();
+db.on('error', function() {
+  console.log('mongoose connection error');
 });
 
-db.once('open', () => {
+db.once('open', function() {
   console.log('mongoose connected successfully');
-  clearTimeout(connectIntervalId);
 });
-
-function connect() {
-  mongoose.connect('mongodb://halfwaze:halfwaze@ds151153.mlab.com:51153/halfwaze', {useMongoClient: true});
-  db = mongoose.connection;
-}
 
 module.exports = db;
