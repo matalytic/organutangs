c701d4be918d1c88febf742a3bf4a542610e8aff
Wei Chen <tengwei1@gmail.com>
2017-08-19 13:09:26 -0400

    maybe

--- server/routes.js
+++ server/routes.js
@@ -90,72 +90,8 @@ var routerInstance = function(io) {
                   console.log('coordinates1', coordinates1);
                   console.log('coordinates2', coordinates2);
 
-                  // get midpoint
-                  // gmaps.generateMidpoint(coordinates1, coordinates2)
-                  //   .then((midpoint) => {
-                  //     console.log('Midpoint generated:', midpoint);
-                  //     // Put midpoint in Yelp API
-                  //     yelp.yelpRequest(midpoint)
-                  //       .then((yelpLocations) => {
-                  //         // Re-render client
-                  //         io.sockets.emit('midpoint', { lat: midpoint.latitude, lng: midpoint.longitude });
-                  //         io.sockets.emit('meeting locations', yelpLocations);
-
-                  //         // formatted as { location1: [lat,lng], location2: [lat, lng] }
-                  //         io.sockets.emit('user locations', {
-                  //           location1: { lat: coordinates1[0], lng: coordinates1[1] },
-                  //           location2: { lat: coordinates2[0], lng: coordinates2[1] }
-                  //         })
-                  //       });
-                  //   });
-
                   // send all points
-                  gmaps.generatePointsAlong(coordinates1, coordinates2, arrivalTime)
-                    .then(({ pointsAlong, midpoint, departure_time }) => {
-
-                      /** send out the departure_time */
-                      io.sockets.emit('departure_time', {
-                        departure_time: departure_time
-                      });
-
-                      // Generate midpoint locations with higher search radius
-                      yelp.yelpRequest(midpoint, 10, 250)
-                        .then((yelpLocations) => {
-                          io.sockets.emit('midpoint', { lat: midpoint.latitude, lng: midpoint.longitude });
-                          io.sockets.emit('mid meeting locations', yelpLocations);
-                          // formatted as { location1: [lat,lng], location2: [lat, lng] }
-                          io.sockets.emit('user locations', {
-                            location1: { lat: coordinates1[0], lng: coordinates1[1] },
-                            location2: { lat: coordinates2[0], lng: coordinates2[1] },
-                          });
-
-                          res.send({
-                            'midpoint': { lat: midpoint.latitude, lng: midpoint.longitude },
-                            'mid_meeting_locations': yelpLocations,
-                            'user locations': {
-                              location1: { lat: coordinates1[0], lng: coordinates1[1] },
-                              location2: { lat: coordinates2[0], lng: coordinates2[1] },
-                            }
-                          });
-                        });
-                      const mappedYelp = pointsAlong.map((point) => {
-                        // points.forEach(point => {
-                        return yelp.yelpRequest(point, 3)
-                          .then((yelpLocations) => {
-                            // Re-render client
-                            return yelpLocations;
-                          });
-                      });
-                      // Generate all restaurants along the path
-                      Promise.all(mappedYelp)
-                        .then((locationsArr) => {
-                          // MERGE ARRAY OF ARRAYS
-                          const allMeetingLocations = [].concat.apply([], locationsArr);
-                          io.sockets.emit('all meeting locations', allMeetingLocations );
-                        })
-                        .catch(err => console.log("Error with promise all"), err);
-                    })
-                    .catch(err => console.log(err));
+                  getLocationsAndSend(coordinates1, coordinates2, arrivalTime, transportation, io);
                   //res.send('Results found.');
                 })
                 .catch(err => console.log("Err getting geocode from Google API"), err);
@@ -172,72 +108,7 @@ var routerInstance = function(io) {
               console.log('coordinates1', coordinates1);
               console.log('coordinates2', coordinates2);
 
-              // get midpoint
-              // gmaps.generateMidpoint(coordinates1, coordinates2)
-              //   .then((midpoint) => {
-              //     console.log('Midpoint generated:', midpoint);
-              //     // Put midpoint in Yelp API
-              //     yelp.yelpRequest(midpoint)
-              //       .then((yelpLocations) => {
-              //         // Re-render client
-              //         io.sockets.emit('midpoint', { lat: midpoint.latitude, lng: midpoint.longitude });
-              //         io.sockets.emit('meeting locations', yelpLocations);
-
-              //         // formatted as { location1: [lat,lng], location2: [lat, lng] }
-              //         io.sockets.emit('user locations', {
-              //           location1: { lat: coordinates1[0], lng: coordinates1[1] },
-              //           location2: { lat: coordinates2[0], lng: coordinates2[1] }
-              //         })
-              //       });
-              //   });
-
-              // send all points
-              gmaps.generatePointsAlong(coordinates1, coordinates2, arrivalTime)
-                .then(({ pointsAlong, midpoint, departure_time }) => {
-
-                  /** send out the departure_time */
-                  io.sockets.emit('departure_time', {
-                    departure_time: departure_time
-                  });
-
-                  // Generate midpoint locations with higher search radius
-                  yelp.yelpRequest(midpoint, 10, 250)
-                    .then((yelpLocations) => {
-                      io.sockets.emit('midpoint', { lat: midpoint.latitude, lng: midpoint.longitude });
-                      io.sockets.emit('mid meeting locations', yelpLocations);
-                      // formatted as { location1: [lat,lng], location2: [lat, lng] }
-                      io.sockets.emit('user locations', {
-                        location1: { lat: coordinates1[0], lng: coordinates1[1] },
-                        location2: { lat: coordinates2[0], lng: coordinates2[1] },
-                      });
-
-                      res.send({
-                        'midpoint': { lat: midpoint.latitude, lng: midpoint.longitude },
-                        'mid_meeting_locations': yelpLocations,
-                        'user locations': {
-                          location1: { lat: coordinates1[0], lng: coordinates1[1] },
-                          location2: { lat: coordinates2[0], lng: coordinates2[1] },
-                        }
-                      });
-                    });
-                  const mappedYelp = pointsAlong.map((point) => {
-                    // points.forEach(point => {
-                    return yelp.yelpRequest(point, 3)
-                      .then((yelpLocations) => {
-                        // Re-render client
-                        return yelpLocations;
-                      });
-                  });
-                  // Generate all restaurants along the path
-                  Promise.all(mappedYelp)
-                    .then((locationsArr) => {
-                      // MERGE ARRAY OF ARRAYS
-                      const allMeetingLocations = [].concat.apply([], locationsArr);
-                      io.sockets.emit('all meeting locations', allMeetingLocations );
-                    })
-                    .catch(err => console.log("Error with promise all"), err);
-                })
-                .catch(err => console.log(err));
+              getLocationsAndSend(coordinates1, coordinates2, arrivalTime, transportation, io);
               //res.send('Results found.');
             })
             .catch(err => console.log("Err getting geocode from Google API"), err);
